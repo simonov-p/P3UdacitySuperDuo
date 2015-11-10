7b8db7f487b5148809f8f72c5b19ba4cc4e8c605
Petr Simonov <simonov-p@ya.ru>
2015-11-10 22:37:51 +0300

    dynamicly change widget size

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/ScoreWidgetIntentService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/ScoreWidgetIntentService.java
@@ -1,5 +1,6 @@
 package barqsoft.footballscores.widget;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -7,7 +8,11 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.widget.RemoteViews;
 
 import barqsoft.footballscores.DatabaseContract;
@@ -85,9 +90,27 @@ public class ScoreWidgetIntentService extends IntentService {
 
         // Perform this loop procedure for each Today widget
         for (int appWidgetId : appWidgetIds) {
-            RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget_score);
+            // find the correct layout based on the widget`s width
+            int widgetWidth = getWidgetWidth(appWidgetManager, appWidgetId);
+            int defaultWidth = getResources().getDimensionPixelSize(R.dimen.widget_score_default_width);
+            int largeWidth = getResources().getDimensionPixelSize(R.dimen.widget_score_large_width);
+            int layoutId;
+            if (widgetWidth >= largeWidth) {
+                layoutId = R.layout.widget_score_large;
+            } else {
+                layoutId = R.layout.widget_score;
+            }
+
+            RemoteViews views = new RemoteViews(getPackageName(), layoutId);
+            // home info
             views.setImageViewResource(R.id.widget_icon, R.drawable.ic_launcher);
-            views.setTextViewText(R.id.score_text_view, homeCol);
+            views.setTextViewText(R.id.score_home_text_view, homeCol + "");
+            views.setTextViewText(R.id.score_away_text_view, awayCol + "");
+            views.setTextViewText(R.id.score_home_goal_text_view, homeGoals + "");
+            views.setTextViewText(R.id.score_away_goal_text_view, awayGoals + "");
+            views.setImageViewResource(R.id.home_icon, R.drawable.manchester_city);
+            views.setImageViewResource(R.id.away_icon, R.drawable.arsenal);
+
 
             // Create an Intent to launch MainActivity
             Intent launchIntent = new Intent(this, MainActivity.class);
@@ -97,11 +120,26 @@ public class ScoreWidgetIntentService extends IntentService {
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
-
-
-
     }
-    private void init(){
 
+    private int getWidgetWidth(AppWidgetManager appWidgetManager, int appWidgetId) {
+        // prior to Jelly Bean, widgets were always their default size
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            return getResources().getDimensionPixelSize(R.dimen.widget_score_default_width);
+        }
+        return getWidgetWidthFromOptions(appWidgetManager, appWidgetId);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private int getWidgetWidthFromOptions(AppWidgetManager appWidgetManager, int appWidgetId) {
+        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+        if (options.containsKey(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)) {
+            int minWidtDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+            // the width returned is in dp, but we`ll convert it to pixels to match the other widths
+            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, minWidtDp,
+                    displayMetrics);
+        }
+        return 0;
     }
 }
--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/ScoreWidgetProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/ScoreWidgetProvider.java
@@ -20,34 +20,17 @@ public class ScoreWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.e("mytag:ScoreWidgetProvider", "onUpdate");
-//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_score);
-//        views.setImageViewResource(R.id.widget_icon, R.drawable.ic_launcher);
-//        views.setTextViewText(R.id.widget_high_temperature, "petr");
-//
-//        // Create an Intent to launch MainActivity
-//        Intent launchIntent = new Intent(context, MainActivity.class);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
-//        views.setOnClickPendingIntent(R.id.widget, pendingIntent);
-//
-//        // Tell the AppWidgetManager to perform an update on the current app widget
-//        appWidgetManager.updateAppWidget(appWidgetIds[0], views);
         context.startService(new Intent(context, ScoreWidgetIntentService.class));
     }
 
     @Override
     public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
                                           int appWidgetId, Bundle newOptions) {
-        Log.e("mytag:ScoreWidgetProvider", "onAppWidgetOptionsChanged");
         context.startService(new Intent(context, ScoreWidgetIntentService.class));
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        Log.e("mytag:ScoreWidgetProvider", "onReceive");
-//        if (SunshineSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())) {
-//            context.startService(new Intent(context, ScoreWidgetIntentService.class));
-//        }
     }
 }
