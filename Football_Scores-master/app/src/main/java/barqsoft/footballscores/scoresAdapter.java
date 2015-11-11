1e2e09c3c81dd1a2bc6a0809e953be2990e3cf0e
Petr Simonov <simonov-p@ya.ru>
2015-11-11 22:51:12 +0300

    content descriptions

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/scoresAdapter.java
@@ -49,13 +49,22 @@ public class ScoresAdapter extends CursorAdapter
         mHolder.home_name.setText(cursor.getString(COL_HOME));
         mHolder.away_name.setText(cursor.getString(COL_AWAY));
         mHolder.date.setText(cursor.getString(COL_MATCHTIME));
-        mHolder.score.setText(Utilies.getScores(cursor.getInt(COL_HOME_GOALS),cursor.getInt(COL_AWAY_GOALS)));
+        mHolder.score.setText(Utilies.getScores(cursor.getInt(COL_HOME_GOALS), cursor.getInt(COL_AWAY_GOALS)));
         mHolder.match_id = cursor.getDouble(COL_ID);
         mHolder.home_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 cursor.getString(COL_HOME)));
         mHolder.away_crest.setImageResource(Utilies.getTeamCrestByTeamName(
                 cursor.getString(COL_AWAY)
         ));
+
+        // Content description
+        mHolder.home_name.setContentDescription(context.getString(R.string.home_name_cd));
+        mHolder.away_name.setContentDescription(context.getString(R.string.away_name_cd));
+        mHolder.date.setContentDescription(context.getString(R.string.date_cd));
+        mHolder.score.setContentDescription(context.getString(R.string.score_cd));
+        mHolder.home_crest.setContentDescription(context.getString(R.string.home_crest_cd));
+        mHolder.away_crest.setContentDescription(context.getString(R.string.away_crest_cd));
+
         //Log.v(FetchScoreTask.LOG_TAG,mHolder.home_name.getText() + " Vs. " + mHolder.away_name.getText() +" id " + String.valueOf(mHolder.match_id));
         //Log.v(FetchScoreTask.LOG_TAG,String.valueOf(detail_match_id));
         LayoutInflater vi = (LayoutInflater) context.getApplicationContext()
@@ -74,6 +83,7 @@ public class ScoresAdapter extends CursorAdapter
             TextView league = (TextView) v.findViewById(R.id.league_textview);
             league.setText(Utilies.getLeague(cursor.getInt(COL_LEAGUE)));
             Button share_button = (Button) v.findViewById(R.id.share_button);
+            share_button.setContentDescription(context.getString(R.string.share_button_cd));
             share_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v)
